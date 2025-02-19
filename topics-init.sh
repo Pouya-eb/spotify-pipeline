8fa0c576eca5b068c103934bdf1a2ed2c6ef5b61
Majid Darigh <majiddarigh@gmail.com>
2025-02-19 12:46:08 +0330

    kafka container added

---
+++ topics-init.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "🔹 Creating Topics in Kafka..."
+/usr/bin/kafka-topics --create --topic eventsim-input --bootstrap-server localhost:9092 --partitions 3 --replication-factor 1
+/usr/bin/kafka-topics --create --topic spark-output --bootstrap-server localhost:9092 --partitions 3 --replication-factor 1
+echo "✅ Topics created successfully!"
