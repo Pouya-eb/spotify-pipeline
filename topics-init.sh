9f299b2ece5e73fdcc187ec9dbf0b8ae10b81096
Majid Darigh <majiddarigh@gmail.com>
2025-02-19 23:48:17 +0330

    topics created by bash script

--- topics-init.sh
+++ topics-init.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# echo " topics-init.sh is running!" >> /tmp/debug.log
 echo "🔹 Creating Topics in Kafka..."
 /usr/bin/kafka-topics --create --topic eventsim-input --bootstrap-server localhost:9092 --partitions 3 --replication-factor 1
 /usr/bin/kafka-topics --create --topic spark-output --bootstrap-server localhost:9092 --partitions 3 --replication-factor 1
